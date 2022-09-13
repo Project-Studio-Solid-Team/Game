92f62ebbb8d9349039c5ab50e3d2c58d117523cb
harmc006 <harmc006@mymail.unisa.edu.au>
2022-09-13 15:02:35 +0930

    Level2 scene made, Tardis player added
    - Level 2 scene made, background placeholder there, awaiting level design from Assets team
    - Tardis added, movement script for Tardis made
    TODO: tons of stuff

---
+++ Assets/Scripts/TardisController2D.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TardisController2D : MonoBehaviour
+{
+    float maxSpeed = 8.0f;
+    float rotSpeed = 250f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  
+    }
+
+    void Update()
+    {
+        // Ship rotations
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z -= Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        rot = Quaternion.Euler(0, 0, z);
+        transform.rotation = rot;
+
+        // Ship movement
+        Vector3 pos = transform.position;
+        Vector3 vel = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
+        pos += rot * vel;
+        transform.position = pos;
+
+    }
+
+
+
+}
