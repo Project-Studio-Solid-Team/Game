ec3346797b3dc92109f9cd6ceaeee60f26c65e60
harmc006 <harmc006@mymail.unisa.edu.au>
2022-09-30 13:49:54 +0930

    NPCs and Collectables integrated
    - Recovered most losses now, just a few to go
    - Level logic 90% implemented, just need to add a few more things

---
+++ Assets/Scripts/Level2/Result2.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Result2 : MonoBehaviour
+{
+    public Questions questions;
+    public GameObject correctSprite;
+    private AudioSource correctSound; 
+    public GameObject incorrectSprite;
+    private AudioSource incorrectSound; 
+    public LevelVariables scores;
+    public Button trueButton;
+    public Button falseButton;
+    public UnityEvent onNextQuestion;
+    public GameObject quizBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        correctSprite.SetActive(false);
+        incorrectSprite.SetActive(false);
+        correctSound = correctSprite.GetComponent<AudioSource>();
+        incorrectSound = incorrectSprite.GetComponent<AudioSource>();
+    }
+
+    public void ShowResults(bool answer)
+    {   
+        correctSprite.SetActive(questions.questionsList[questions.currentQuestion].isTrue == answer); 
+        incorrectSprite.SetActive(questions.questionsList[questions.currentQuestion].isTrue != answer);
+
+        if(questions.questionsList[questions.currentQuestion].isTrue == answer)
+        {
+            scores.AddScore();
+            correctSound.Play();
+        }
+        else
+        {
+            incorrectSound.Play();
+        }
+
+        trueButton.interactable = false;
+        falseButton.interactable = false;
+
+        StartCoroutine(ShowResult());
+    }
+
+    private IEnumerator ShowResult()
+    {
+        quizBox.SetActive(false);
+        yield return new WaitForSeconds(1.0f);
+        correctSprite.SetActive(false);
+        incorrectSprite.SetActive(false);
+        
+
+        trueButton.interactable = true;
+        falseButton.interactable = true;
+
+        onNextQuestion.Invoke();
+    }
+
+}
