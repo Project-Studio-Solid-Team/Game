8923adf5ee8d7e09f4ee06ababdc8a1ff8c1532d
An Truong <truan004@mymail.unisa.edu.au>
2022-10-05 00:05:38 +1030

    Add leaderboard

---
+++ Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,15 @@
+public class PlayerObject {
+    public string name;
+    public int rank;
+    public string classID;
+    public int score;
+
+    public PlayerObject(string name, int score, string classID, int rank) {
+        this.name = name;
+        this.score = score;
+        this.classID = classID;
+        this.rank = rank;
+    }
+
+
+}
