6aabf537e534a6f97258297cbf50ff2826ee385b
Jye Mathew <mathewjye@gmail.com>
2022-10-04 19:33:29 +1030

    Fixing WebFunctions

--- Assets/Scripts/Web.cs
+++ Assets/Scripts/Web.cs
@@ -12,9 +12,9 @@ public class Web : MonoBehaviour
 	string url = string.Format("http://{0}:{1}/unity_", webserverIp, webserverPort);
 	
     
-    IEnumerator sendScore(string username, int score)
+    IEnumerator sendScore(string username, int score, int levelid)
     {	
-        string sendUrl = url + string.Format("submitscore?username={0}&score={1}", username, score);
+        string sendUrl = url + string.Format("submitscore?username={0}&score={1}&levelid={2}", username, score,levelid);
     	UnityWebRequest www = UnityWebRequest.Get(sendUrl);
         yield return www.SendWebRequest();
 
@@ -27,8 +27,8 @@ public class Web : MonoBehaviour
     
     }
 
-    IEnumerator getScores(string username, int scorecount){
-        string sendUrl = url + string.Format("displayscore?username={0}&scorecount={1}", username,scorecount);
+    IEnumerator<string> getScores(string username, int scorecount){
+        string sendUrl = url + string.Format("displayscore?username={0}&scorecount={1}&levelid={2}", username,scorecount,levelid);
         UnityWebRequest www = UnityWebRequest.Get(sendUrl);
         yield return www.SendWebRequest();
 
@@ -40,6 +40,7 @@ public class Web : MonoBehaviour
             //this is a bit messy and i could do it over 2 steps but just converts the input byte array to a string
             //then uses the string split function with a , as the delimiter to make an array of strings
             string[] scores = System.Text.Encoding.Default.GetString(results).Split(",");
+            return scores;
 
             //at present will be array 
             //userTopScore:VALUE
