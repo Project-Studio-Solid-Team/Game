a7f6fd71817588faca45ad13ed55c84afc073d90
Jye Mathew <mathewjye@gmail.com>
2022-10-04 23:41:31 +1030

    Split user text

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
-    public GameObject rank;
     public GameObject user;
     public GameObject group;
     public GameObject score;
 
+    private string[10] usernames;
+    private string[10] scores;
+    private string[10] classes;
+
     // Start is called before the first frame update
     void Start()
     {
-        //Webinteraction = new Web();
-        //string[] temp = Webinteraction.getScores(PlayerPrefs.GetString("usernamePlayer"), 10, 1);
+        Web Webinteraction = new Web();
+        string[] temp = Webinteraction.getScores(PlayerPrefs.GetString("usernamePlayer"), 10, 1);
 
-        /*foreach(string temp2 in temp){ 
-            Debug.Log(temp2);
-        }*/
+        int position =0;
+        foreach(string temp2 in temp){ 
+
+           string[] out = temp2.Split(":");
+           usernames[index] = out[0];
+           scores[index] = out[1];
+           classes[index] = out[2];
+        }
 
         Transform[] ranks = rank.GetComponentsInChildren<Transform>();
         foreach (Transform child in ranks)
