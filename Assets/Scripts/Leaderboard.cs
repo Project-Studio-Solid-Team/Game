b5cfcb7ddae1c0a491e688eabfa3b69d9c1871fd
harmc006 <harmc006@mymail.unisa.edu.au>
2022-10-04 23:14:22 +1030

    Pushing Leaderboard Changes
    Stuff for Jye

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
+    public GameObject rank;
+    public GameObject user;
+    public GameObject group;
+    public GameObject score;
+
     // Start is called before the first frame update
     void Start()
     {
-        Web Webinteraction = new Web();
-        string[] temp = Webinteraction.getScores(PlayerPrefs.GetString("usernamePlayer"), 10, 1);
+        //Webinteraction = new Web();
+        //string[] temp = Webinteraction.getScores(PlayerPrefs.GetString("usernamePlayer"), 10, 1);
 
-        foreach(string temp2 in temp){ 
+        /*foreach(string temp2 in temp){ 
             Debug.Log(temp2);
-        }
-        
-    }
+        }*/
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Transform[] ranks = rank.GetComponentsInChildren<Transform>();
+        foreach (Transform child in ranks)
+        {
+            Debug.Log(child);
+        }
+
     }
-}
+}   
